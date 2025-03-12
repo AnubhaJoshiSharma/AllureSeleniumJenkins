6e2a2f33fd3b473cef94fd4f4194c37a3fc63808
anubh <anubha.joshi.2008@gmail.com>
2025-03-12 00:44:12 -0400

    Added @Description, @Severity, and for @Test of testNG added another description

--- src/test/java/com/mavenJenkinsAllure/GoogleTests.java
+++ src/test/java/com/mavenJenkinsAllure/GoogleTests.java
@@ -4,11 +4,14 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import basePackage.BaseClass;
+import io.qameta.allure.Description;
 import io.qameta.allure.Step;
 
 public class GoogleTests extends BaseClass {
+	
+	@Description("TEST NUMBERED 1")
 	@Step("Chrome browser test for google.com")
-	@Test
+	@Test(description="This is method 1 for opening google")
 	public void testMethod1() {
 		System.out.println("Inside method 1");
 		try {
@@ -19,8 +22,9 @@ public class GoogleTests extends BaseClass {
 		}
 		Assert.assertEquals(getDriver().getCurrentUrl(),"https://www.google.com/","SOME MESSAGE");
 	}
+	@Description("TEST NUMBERED 2")
 	@Step("Firefox browser test for Google.com")
-	@Test
+	@Test(description="This is method 1 for opening google")
 	public void testMethod2() {
 		System.out.println("Inside method 2");
 		try {
